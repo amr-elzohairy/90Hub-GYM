bc17f406c27d1d4cedb9a9f218c4284cf61a67df
Amr-Elzohairy <aelzohairy9@gmail.com>
2024-10-15 10:01:53 +0300

    app bar done

--- lib/features/community/ui/community_screen.dart
+++ lib/features/community/ui/community_screen.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hubmaster/core/themes/app_colors.dart';
 
 class CommunityScreen extends StatelessWidget {
   const CommunityScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.scaffoldBgColor,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Padding(
+            padding: EdgeInsets.only(top: 5.h),
+            child: const Icon(
+              Icons.dehaze_sharp,
+              color: Color(0xff505050),
+            ),
+          ),
+        ),
+        title: Text(
+          'COMMUNITY',
+          style: Theme.of(context).textTheme.displayMedium!.copyWith(
+              fontSize: 22.sp,
+              fontFamily: "IntegralCF",
+              fontWeight: FontWeight.w400),
+        ),
+        actions: [
+          Padding(
+            padding:  EdgeInsets.only(top: 5.h),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: SvgPicture.asset("assets/svgs/search.svg"),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: SvgPicture.asset("assets/svgs/Notification.svg"),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
